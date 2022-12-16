dd3c0ca9ab682206d74d3ed49b40ee29c897e0bf
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-12-16 08:27:59 +0000

    Created base express server in server repo

---
+++ server/express-server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan'); //Outputs request data
+
+//////////////////////////////////
+//Configuration
+/////////////////////////////////
+const app = express();
+const PORT = 7777;
+
+//////////////////////////////////
+//Middleware
+/////////////////////////////////
+
+app.use(express.urlencoded({extended: true})); //for form submission
+app.use(morgan('dev'));
+
+//////////////////////////////////
+//Listener - For server to listen for/be open to requests
+/////////////////////////////////
+
+app.listen(PORT, () => {
+  console.log('Express app is listening on port:', PORT)
+})
+
+//////////////////////////////////
+//Routes
+/////////////////////////////////
