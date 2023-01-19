af2494de0566f02e96b4a27447f012ab7bbefdbb
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-19 14:41:02 -0500

    Refactored order of schema and seed queries.

--- db/schema/create.sql
+++ db/schema/create.sql
@@ -9,14 +9,6 @@ CREATE TABLE conversation (
   id SERIAL PRIMARY KEY NOT NULL,
   conversation_name VARCHAR(250)
 );
--- Use of TIMESTAMPTZ allows for value to be stored as UTC and automatically adjusts when db experiences timezone change
-CREATE TABLE message (
-  id SERIAL PRIMARY KEY NOT NULL,
-  contact_id INTEGER REFERENCES contact(id) ON DELETE CASCADE,
-  message_text VARCHAR(2000) NOT NULL,
-  sent_datetime VARCHAR(255) NOT NULL, 
-  conversation_id INTEGER REFERENCES conversation(id) ON DELETE CASCADE
-);
 
 -- Might need to adjust passwrd hash length from 255, as hash might be more then this!
 CREATE TABLE contact (
@@ -27,6 +19,14 @@ CREATE TABLE contact (
   email VARCHAR(255) NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
   profile_photo_url VARCHAR(255) NOT NULL
+);
+-- Use of TIMESTAMPTZ allows for value to be stored as UTC and automatically adjusts when db experiences timezone change
+CREATE TABLE message (
+  id SERIAL PRIMARY KEY NOT NULL,
+  contact_id INTEGER REFERENCES contact(id) ON DELETE CASCADE,
+  message_text VARCHAR(2000) NOT NULL,
+  sent_datetime VARCHAR(255) NOT NULL, 
+  conversation_id INTEGER REFERENCES conversation(id) ON DELETE CASCADE
 );
 
 CREATE TABLE group_member (
