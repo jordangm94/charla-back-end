fb046d96344740bc0baf1084605b270b724cb337
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-19 11:44:58 -0500

    Created conversation table of chat app.

--- db/schema/create.sql
+++ db/schema/create.sql
@@ -1,8 +1,15 @@
+DROP TABLE IF EXISTS conversation CASCADE;
 DROP TABLE IF EXISTS message CASCADE;
 
+-- Must determine if we want conversation name to be mandatory or not, would be good for groups but not for individual conversations
+CREATE TABLE conversation (
+  id SERIAL PRIMARY KEY NOT NULL,
+  conversation_name VARCHAR(250)
+);
 -- Use of TIMESTAMPTZ allows for value to be stored as UTC and automatically adjusts when db experiences timezone change
 CREATE TABLE message (
   id SERIAL PRIMARY KEY NOT NULL,
   message_text VARCHAR(2000) NOT NULL,
   sent_datetime TIMESTAMPTZ, 
+  conversation_id INTEGER REFERENCES conversation(id) ON DELETE CASCADE
 );
