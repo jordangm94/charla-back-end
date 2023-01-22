55f905fd04607e353d070a8bb698aa25d2e9ff1d
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-22 16:12:49 -0500

    Finished application back end for routes

--- server/application2.js
+++ server/application2.js
@@ -26,3 +26,41 @@ function read(file) {
     );
   });
 }
+
+module.exports = function application(
+  ENV
+) {
+  app.use(cors());
+  app.use(helmet());
+  app.use(bodyparser.json());
+
+  app.use("/api", routes(db));
+
+  //////////
+
+  if (ENV === "development" || ENV === "test") {
+    Promise.all([
+      read(path.resolve(__dirname, `db/schema/create.sql`)),
+      read(path.resolve(__dirname, `db/schema/${ENV}.sql`))
+    ])
+      .then(([create, seed]) => {
+        app.get("/api/debug/reset", (request, response) => {
+          db.query(create)
+            .then(() => db.query(seed))
+            .then(() => {
+              console.log("Database Reset");
+              response.status(200).send("Database Reset");
+            });
+        });
+      })
+      .catch(error => {
+        console.log(`Error setting up the reset route: ${error}`);
+      });
+  }
+
+  app.close = function() {
+    return db.end();
+  };
+
+  return app;
+};
