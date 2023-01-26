be789d906dfc35afd47cb13a13148d20a936cc14
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-25 21:56:12 -0300

    Create a login post route without code yet

--- server/routes/chat-app-router.js
+++ server/routes/chat-app-router.js
@@ -9,5 +9,9 @@ module.exports = db => {
   router.get('/', (req, res) => {
     res.send('Hello from the CHAT APP!');
   });
+
+  router.post('/login', (req, res) => {
+
+  });
   return router;
-}
+};
