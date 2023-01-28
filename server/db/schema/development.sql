a2c877f76a30840bc797a9a21fb46d2e44d2d86a
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-27 20:07:26 -0500

    Changed Values in SEEDS for Profile Pics

--- server/db/schema/development.sql
+++ server/db/schema/development.sql
@@ -1,8 +1,8 @@
 DELETE from contact;
 
 INSERT INTO contact (first_name, last_name, user_name, email, password_hash, profile_photo_url)
-VALUES ('Jordan', 'Guerrero Martinez', 'jordangm94', 'jordan.guerrero.martinez@gmail.com', '12345678', 'https://media.licdn.com/media/AAYQAQSOAAgAAQAAAAAAAB-zrMZEDXI2T62PSuT6kpB6qg.png'),
-('Jae Yun', 'Jeong', 'alex94', 'jeongalex0616@gmail.com', '12345678', 'https://media.licdn.com/dms/image/D4E35AQGThx0CezufGA/profile-framedphoto-shrink_800_800/0/1670789621093?e=1674763200&v=beta&t=-mxVYo3312r54JMyX7wx9DLPaGIUpCU0MpyudRL-7Sc'),
+VALUES ('Jordan', 'Guerrero Martinez', 'jordangm94', 'jordan.guerrero.martinez@gmail.com', '12345678', 'https://avatars.githubusercontent.com/u/111808635?v=4'),
+('Jae Yun', 'Jeong', 'alex94', 'jeongalex0616@gmail.com', '12345678', 'https://avatars.githubusercontent.com/u/109401369?v=4'),
 ('Alessia', 'G', 'AlessiaG94', 'Alessiag94@gmail.com', '12345678', 'https://media.istockphoto.com/id/1311084168/photo/overjoyed-pretty-asian-woman-look-at-camera-with-sincere-laughter.jpg?b=1&s=170667a&w=0&k=20&c=XPuGhP9YyCWquTGT-tUFk6TwI-HZfOr1jNkehKQ17g0=');
 
 DELETE from conversation;
