0e570c5a7f324a2cbc306b4d80a704c941944de6
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2023-01-28 21:36:26 -0500

    Used distinct on in query to get latest convo msgs

--- server/routes/chat-app-router.js
+++ server/routes/chat-app-router.js
@@ -15,13 +15,14 @@ module.exports = db => {
 
   router.get('/chat/list', (req, res) => {
     db.query(
-      `SELECT conversation.id as conversation_id, conversation_name, member_1, member_2, message.id AS message_id, message_text, contact.id AS contact_id, contact.first_name, contact.last_name, contact.profile_photo_url, contact.email
+      `SELECT DISTINCT ON (conversation.id) conversation_id, conversation_name, member_1, member_2, message.id AS message_id, message_text, contact.id AS contact_id, contact.first_name, contact.last_name, contact.profile_photo_url, contact.email
 
       FROM conversation JOIN message ON conversation.id = conversation_id JOIN contact ON contact_id = contact.id
-    
+      
       WHERE member_1 = 1 OR member_2 = 1
-          
-      ORDER BY conversation_id ASC, message.id DESC;
+      
+      ORDER BY conversation.id DESC, message.id DESC;
+      ;
       `
     ).then(({ rows }) => {
       res.json(rows);
