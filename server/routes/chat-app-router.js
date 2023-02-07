d6e7228f8093fec539eb81bbd46ea26529ab1e1b
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-02-07 20:35:18 -0300

    Import validateToken

--- server/routes/chat-app-router.js
+++ server/routes/chat-app-router.js
@@ -2,7 +2,7 @@ const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const { createToken } = require("../JWT");
+const { createToken, validateToken } = require("../JWT");
 
 /////////////////////////////////
 /// Index
