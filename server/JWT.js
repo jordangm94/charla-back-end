9c278120babb5e7dcf97d640cb8e078ca01655aa
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-02-06 22:50:54 -0300

    Create a validateToken function

--- server/JWT.js
+++ server/JWT.js
@@ -9,4 +9,21 @@ const createToken = (contact) => {
   return accessToken;
 };
 
+const validateToken = (req, res, next) => {
+  const accessToken = req.session.accessToken;
+
+  if (!accessToken) return res.status(400).json({ error: "User not authenticated" });
+
+  try {
+    const validToken = verify(accessToken, process.env.JWTSECRET);
+
+    if (validToken) {
+      req.authenticated = true;
+      return next();
+    }
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
+
 module.exports = { createToken };
