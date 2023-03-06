1ee332fb8063c3b5f80a4c40b3ba6bd936cbce07
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-03-05 22:27:23 -0300

    Create new table in psql for the sessions

--- server/db/schema/create.sql
+++ server/db/schema/create.sql
@@ -40,8 +40,18 @@ CREATE TABLE feedback (
   id SERIAL PRIMARY KEY NOT NULL,
   full_name VARCHAR(255) NOT NULL,
   message VARCHAR(2000) NOT NULL 
-)
+);
 
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
 -- CREATE TABLE group_member (
 --   id SERIAL PRIMARY KEY NOT NULL,
 --   conversation_id INTEGER REFERENCES conversation(id),
